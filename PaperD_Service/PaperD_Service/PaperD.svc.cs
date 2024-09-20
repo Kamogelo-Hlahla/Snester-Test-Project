79d2f2b30873e27cf26dae0fdbca40f5c1008e9d
Kamogelo Hlahla <Kamogelohlahla16@gmail.com>
2024-09-20 16:45:16 +0200

    Add files via upload

---
+++ PaperD_Service/PaperD_Service/PaperD.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PaperD_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PaperD" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select PaperD.svc or PaperD.svc.cs at the Solution Explorer and start debugging.
+    public class PaperD : IPaperD
+    {
+        PaperDDataContext db = new PaperDDataContext();
+        public SysUser Login(string Email, string Password)
+        {
+            var user = (from u in db.SysUsers
+                        where u.Email.Equals(Email) &&
+                        u.Password.Equals(Password)
+                        select u).FirstOrDefault();
+
+            var LoginUser = new SysUser
+            {
+                Id = user.Id,
+                Name = user.Name,
+                UserType = user.UserType
+            };
+
+            return LoginUser;
+        }
+    }
+}
